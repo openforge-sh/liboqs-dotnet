aa8fb7e3096990494d5ee99d309935899821c2f5
Kamal Tufekcic <kamal.tufekcic@ugs.ro>
2025-09-16 00:52:03 +0300

    initial commit

--- tests/Full.Tests/LibOqsCollectionDefinition.cs
+++
@@ -1,9 +0,0 @@
-using OpenForge.Cryptography.LibOqs.Tests.Common;
-using Xunit;
-
-namespace OpenForge.Cryptography.LibOqs.Tests;
-
-[CollectionDefinition("LibOqs Collection")]
-public sealed class LibOqsCollectionDefinition : ICollectionFixture<LibOqsTestFixture>
-{
-}
--- tests/KEM.Tests/LibOqsCollectionDefinition.cs
+++
@@ -1,9 +0,0 @@
-using OpenForge.Cryptography.LibOqs.Tests.Common;
-using Xunit;
-
-namespace OpenForge.Cryptography.LibOqs.KEM.Tests;
-
-[CollectionDefinition("LibOqs Collection")]
-public sealed class LibOqsCollectionDefinition : ICollectionFixture<LibOqsTestFixture>
-{
-}
