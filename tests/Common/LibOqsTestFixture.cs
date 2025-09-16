3c003fdd6da61a8a748417a8e3af2dd06e72650f
Kamal Tufekcic <kamal.tufekcic@ugs.ro>
2025-09-16 21:36:28 +0300

    acts of desperation 4

--- tests/Common/LibOqsTestFixture.cs
+++ tests/Common/LibOqsTestFixture.cs
@@ -5,31 +5,21 @@ namespace OpenForge.Cryptography.LibOqs.Tests.Common;
 public sealed class LibOqsTestFixture : IDisposable
 {
     private static readonly object _lock = new();
-    private static int _referenceCount;
+    private static bool _initialized;
 
     public LibOqsTestFixture()
     {
         lock (_lock)
         {
-            if (_referenceCount == 0)
+            if (!_initialized)
             {
                 OqsCore.Initialize();
+                _initialized = true;
             }
-
-            _referenceCount++;
         }
     }
 
     public void Dispose()
     {
-        lock (_lock)
-        {
-            _referenceCount--;
-
-            if (_referenceCount == 0)
-            {
-                OqsCore.Destroy();
-            }
-        }
     }
 }
